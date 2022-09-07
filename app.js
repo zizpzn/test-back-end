be425a0804a1b68d78d73567da25c75eb42f4f71
zizpzn <zizpzn@gmail.com>
2022-09-07 16:50:28 +0700

    02

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(8000, () => {
-  console.log(`Server is running on http://localhost:8000`);
+app.listen(80, () => {
+  console.log(`Server is running on http://localhost:80`);
 });
 
 app.get("/", (req, res) => {
